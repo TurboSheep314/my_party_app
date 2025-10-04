deff0a03d1e15221c59c2d13270df5c2b241c591
TurboSheep314 <51458648+TurboSheep314@users.noreply.github.com>
2025-10-04 13:08:27 -0400

    added import json

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 import gspread
+import json
 from oauth2client.service_account import ServiceAccountCredentials
 
 # ---------- Google Sheets Setup ----------
